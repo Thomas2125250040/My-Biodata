c07287f7bc1b36bf9eac2f0d68b250edefe40eae
Thomas2125250040 <113816448+Thomas2125250040@users.noreply.github.com>
2023-02-23 11:13:27 +0700

    Add login and Logout
    - SharedPreference for login]
    - User can login and logout

--- app/src/main/java/com/masteru/mybiodata/MainActivity.java
+++ app/src/main/java/com/masteru/mybiodata/MainActivity.java
@@ -2,13 +2,44 @@ package com.masteru.mybiodata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvUsername;
+    Button btnLogout;
+
+    private Utilities utilities = new Utilities();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvUsername = findViewById(R.id.tv_username);
+        btnLogout = findViewById(R.id.btn_logout);
+
+        if (!utilities.isLogin(this,"xUsername")){
+            Intent intent = new Intent(this, activity_login.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            String username = utilities.getPref(this, "xUsername");
+            tvUsername.setText(username);
+        }
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                utilities.setPref(MainActivity.this, "xUsername", null);
+                Intent intent = new Intent(MainActivity.this, activity_login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
